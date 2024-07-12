ffe5c3f2ef9f939a9310f827b8fafff8ead110a4
Nikhilesh <nikhileshg02@gmail.com>
2024-07-12 12:59:21 +0530

    ui fixes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
-      <div className="mx-auto pb-20">
+      <div className="mx-auto pb-12">
         <Navbar />
         <div className="pt-20"></div>
         <div className="px-8">
