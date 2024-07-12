9c1e59b9796867ac9d0e59b12f90ea79b956f71b
Nikhilesh <nikhileshg02@gmail.com>
2024-07-12 12:36:34 +0530

    captcha added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ function Hero() {
             </motion.p>
 
             <div className="mt-16">
-              <a href={import.meta.env.VITE_RESUME} target="__blank" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2">
+              <a href={import.meta.env.VITE_RESUME_LINK} target="__blank" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2">
                 <LuDownload className="inline-block text-center text-2xl pb-1 me-1 "/>Resume
               </a>
             </div>
